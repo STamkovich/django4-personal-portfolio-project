a509f3a7d82e9898c24192f48eeda022b4a28a34
STamkovich <tamkovich1072@gmail.com>
2022-05-13 23:54:51 +0300

    'two commit'

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    """Мои посты"""
+    title_post = models.CharField("Название поста", max_length=200)
+    post_info = models.TextField("Информация")
+    date = models.DateField(null=True)
+
+    def __str__(self):
+        return self.title_post
--- portfolio/models.py
+++ portfolio/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
-    image = models.ImageField(upload_to='portfolio/images/')
-    url = models.URLField(blank=True)
+    """Моя проекты"""
+    title = models.CharField("Название проекта", max_length=100)
+    description = models.CharField("Описание проекта", max_length=250)
+    image = models.ImageField("Изображение", upload_to='portfolio/images/')
+    url = models.URLField("Ссылка на проекта", blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
